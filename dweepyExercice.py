1a890e20ebafc2fb4df7d88b06f1528a17b7af90
ArmandoTapia <rodney.tapia@epn.edu.ec>
2020-08-17 10:25:42 -0500

    set of hit the webhook

--- dweepyExercice.py
+++ dweepyExercice.py
@@ -6,8 +6,14 @@ from twisted.internet import task, reactor
 ## Define time
 timeout = 60.0  # Sixty seconds
 
+## 15 Minutes
+# timeout = 60.0 * 15  # Sixty seconds
+
 ## URL to make post 
 urlComplete='http://localhost:5000/tempHum'
+#urlComplete='https://backendtodolegal.herokuapp.com/tempHum'
+
+urlWebHook='https://webhook.site/bb5a6abe-1a7b-452e-86c8-40b2bbba00b7'
 
 ## Lambda function
 dict_lamb = lambda: dweepy.get_dweets_for('thecore')[0]
@@ -20,11 +26,13 @@ def doWork():
     hum = dict_lamb()['content']['humidity']
     infoTempHum = {"temperature":tempC,"humidity":hum}
     response = requests.post(urlComplete, json = infoTempHum)
+    #hit the webhook 
+    responseWebHook = requests.post(urlWebHook, json = infoTempHum)
     print(response.json()) 
     pass
 
 l = task.LoopingCall(doWork)
-l.start(timeout) # call every sixty seconds
+l.start(timeout) 
 
 reactor.run()
 
